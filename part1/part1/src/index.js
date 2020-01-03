b68a8d1715cb40f67f70b585250d453446435981
HalvallaAwaits <joereim@gmail.com>
2020-01-03 16:37:20 -0500

    event handler partial completion

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from "react-dom";
 const App = props => {
   const [value, setValue] = useState(10);
 
+  const setToValue = newValue => () => {
+    setValue(newValue);
+  };
+
   return (
     <div>
       {value}
-      <button>reset to zero</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>zero</button>
+      <button onClick={setToValue("hi")}>hi</button>
     </div>
   );
 };
