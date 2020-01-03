80f331dd4539f0ea4ab36f3c9b13f524121e69c3
HalvallaAwaits <joereim@gmail.com>
2020-01-03 14:28:38 -0500

    part1c
    part1c complete

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom";
 
 const App = props => {
   const [counter, setCounter] = useState(0);
+  const setToValue = value => () => setCounter(value);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={setToValue(counter + 1)} text="increment" />
+      <Button onClick={setToValue(counter - 1)} text="decrement" />
+      <Button onClick={setToValue(0)} text="reset" />
+    </div>
+  );
+};
 
-  return <div>{counter}</div>;
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
