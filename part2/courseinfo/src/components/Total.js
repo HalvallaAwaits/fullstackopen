4739d149442f1a8b6650de994d09958da2a6c77d
HalvallaAwaits <joereim@gmail.com>
2020-02-03 15:34:35 -0500

    2.3 complete, reduce() used for total

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,14 +1,12 @@
 ﻿import React from "react";
 
 const Total = ({ course }) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {course.parts[0].exercises +
-        course.parts[1].exercises +
-        course.parts[2].exercises}
-    </p>
+  const totalExercises = course.parts.reduce(
+    (acc, cur) => acc + cur.exercises,
+    0
   );
+
+  return <p>Number of exercises {totalExercises}</p>;
 };
 
 export default Total;
