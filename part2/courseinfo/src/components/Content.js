9f6ef92aeed48733514c449d1765a0e24dda8794
HalvallaAwaits <joereim@gmail.com>
2020-02-03 15:50:20 -0500

    2.4 and 2.5 complete

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,23 +1,14 @@
 ﻿import React from "react";
 import Part from "./Part";
 
-const Content = props => {
-  return (
-    <div>
-      <Part
-        part={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        part={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        part={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </div>
-  );
+const Content = ({ course }) => {
+  const rows = () => {
+    return course.parts.map(part => (
+      <Part key={part.id} part={part.name} exercises={part.exercises} />
+    ));
+  };
+
+  return <div>{rows()}</div>;
 };
 
 export default Content;
